e6ca560c22f0d6d3078e17c0f0b7ed9eea1e8a4f
Titou-Onee <tmauchamp.0@gmail.com>
2026-04-22 09:47:26 +0200

    fix backend-init

--- backend-init.groovy
+++ backend-init.groovy
@@ -1,5 +1,5 @@
 pipeline {
-    agent{
+    agent {
         label "agent_1"
     }
     environment {
@@ -8,49 +8,52 @@ pipeline {
     }
 
     stages {
-        stage('Fetch Secrets') {
+        stage('Bootstrap Backend') {
             steps {
                 withVault(configuration: [
-                disableChildPoliciesOverride: false, 
-                vaultCredentialId: 'Vault_jenkins_terraform', 
-                vaultUrl: 'http://vault:8200'
-            ], vaultSecrets: [
-                [path: 'secret/scaleway/access', engineVersion: 2, secretValues: [
-                    [envVar: 'SCW_ACCESS_KEY', vaultKey: 'access_key'],
-                    [envVar: 'SCW_SECRET_KEY', vaultKey: 'secret_key'],
-                    [envVar: 'SCW_PROJECT_ID', vaultKey: 'project_id']
-                ]]
-            ]) {
-                script {
-                    sh '''
-                        # On définit la variable par défaut pour la CLI scw
-                        export SCW_DEFAULT_PROJECT_ID="$SCW_PROJECT_ID"
-                        
-                        echo "Vérification/Création du bucket S3..."
-                        # On utilise les variables d'environnement injectées par withVault
-                        scw object bucket create name="$BUCKET_NAME" acl=private || true
-                        
-                        echo "Activation du versioning..."
-                        curl -X PUT "https://s3.${SCW_REGION}.scw.cloud/${BUCKET_NAME}?versioning" \
-                            -H "Authorization: Bearer $SCW_SECRET_KEY" \
-                            -d '<VersioningConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><Status>Enabled</Status></VersioningConfiguration>'
-                    '''
+                    disableChildPoliciesOverride: false, 
+                    vaultCredentialId: 'Vault_jenkins_terraform', 
+                    vaultUrl: 'http://vault:8200' // Assure-toi que Vault est UNSEALED
+                ], vaultSecrets: [
+                    [path: 'secret/scaleway/access', engineVersion: 2, secretValues: [
+                        [envVar: 'SCW_ACCESS_KEY', vaultKey: 'access_key'],
+                        [envVar: 'SCW_SECRET_KEY', vaultKey: 'secret_key'],
+                        [envVar: 'SCW_PROJECT_ID', vaultKey: 'project_id']
+                    ]]
+                ]) {
+                    script {
+                        sh '''
+                            # On passe les variables Jenkins au Shell proprement
+                            export REGION="$SCW_REGION"
+                            export BUCKET="$BUCKET_NAME"
+                            export SCW_DEFAULT_PROJECT_ID="$SCW_PROJECT_ID"
+                            
+                            echo "--- Création du bucket S3 : $BUCKET ---"
+                            scw object bucket create name="$BUCKET" region="$REGION" acl=private || true
+                            
+                            echo "--- Activation du versioning ---"
+                            # Utilisation de la clé secrète récupérée de Vault
+                            curl -X PUT "https://s3.${REGION}.scw.cloud/${BUCKET}?versioning" \
+                                -H "Authorization: Bearer $SCW_SECRET_KEY" \
+                                -d '<VersioningConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><Status>Enabled</Status></VersioningConfiguration>'
+                        '''
+                    }
                 }
-            }
-                        
-            writeFile file: 'backend_config.tf', text: """
-                terraform {
-                backend "s3" {
-                    bucket                      = "${BUCKET_NAME}"
-                    key                         = "global/terraform.tfstate"
-                    region                      = "${SCW_REGION}"
-                    endpoint                    = "https://s3.${SCW_REGION}.scw.cloud"
-                    skip_credentials_validation = true
-                    skip_region_validation      = true
-                    skip_requesting_account_id  = true
-                }
-                }
-                """
+                
+                // Génération du fichier TF (Hors du bloc sh pour utiliser l'interpolation Groovy)
+                writeFile file: 'backend_config.tf', text: """
+terraform {
+  backend "s3" {
+    bucket                      = "${BUCKET_NAME}"
+    key                         = "global/terraform.tfstate"
+    region                      = "${SCW_REGION}"
+    endpoint                    = "https://s3.${SCW_REGION}.scw.cloud"
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+  }
+}
+""".stripIndent()
             }
         }        
         stage('Archive Artifacts') {
