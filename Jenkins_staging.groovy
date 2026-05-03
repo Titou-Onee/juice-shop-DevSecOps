d5fc10dcc5eb724ed7889b6c0f4220ae80354995
Titou-Onee <tmauchamp.0@gmail.com>
2026-05-03 10:07:39 +0200

    Added Jenkins staging

---
+++ Jenkins_staging.groovy
@@ -0,0 +1,85 @@
+pipeline{
+    agent{
+        label "agent_1"
+    }
+    options {
+        skipDefaultCheckout()
+    }
+    parameters{
+        string(name: 'IMAGE_DIGEST', defaultValue: '', description: 'Image digest')
+        string(name: 'IMAGE_TAG', defaultValue: '', description: 'Image tag')
+        string(name: 'IMAGE_NAME', value: env.IMAGE_NAME),
+        string(name: 'REGISTRY', value: env.REGISTRY)
+    }
+    environment{
+
+        VAULT_URL= "https://vault:8200"
+        COSIGN_EXPERIMENTAL = "0"
+        COSIGN_KEY = "hashivault://cosign"
+    }   
+    stages{   
+        stage('Verify Signature') {
+                steps {
+                    withVault(configuration: [disableChildPoliciesOverride: false, engineVersion: 2, timeout: 60, vaultCredentialId: 'Jenkins_push', vaultUrl: 'https://vault:8200'], 
+                    vaultSecrets: [
+                        [path: 'secret/scaleway/jenkins_push', secretValues: [[envVar: 'REGISTRY', vaultKey: 'registry']]],
+                        [path: 'secret/cosign/keys', secretValues: [[envVar: 'ROLE_ID', vaultKey: 'role_id'], [envVar: 'SECRET_ID', vaultKey: 'secret_id']]]
+                    ]) {                
+                        sh '''
+                            export VAULT_ADDR="$VAULT_URL"
+
+                            VAULT_TOKEN=$(curl -sf \
+                                --request POST \
+                                --cacert /usr/local/share/ca-certificates/my-internal-ca.crt \
+                                --data "{\\"role_id\\":\\"${ROLE_ID}\\",\\"secret_id\\":\\"${SECRET_ID}\\"}" \
+                                "${VAULT_ADDR}/v1/auth/approle/login" \
+                                | jq -r '.auth.client_token')
+                            
+                            export VAULT_TOKEN
+                            export TRANSIT_SECRET_ENGINE_PATH="transit"
+
+                            cosign verify \
+                                --key "$COSIGN_KEY" \
+                                --allow-insecure-registry=false \
+                                --insecure-ignore-tlog \
+                                "$IMAGE_FULL_REF@$IMAGE_DIGEST"
+
+                            cosign verify-attestation \
+                                --key "$COSIGN_KEY" \
+                                --insecure-ignore-tlog \
+                                --type cyclonedx \
+                                "${IMAGE_FULL_REF}@${IMAGE_DIGEST}"
+                            
+                            curl -sf -H "X-Vault-Token: $VAULT_TOKEN" \
+                                --cacert /usr/local/share/ca-certificates/my-internal-ca.crt \
+                                -X POST "$VAULT_ADDR/v1/auth/token/revoke-self" || true
+                        '''
+                    }
+
+                }
+        }
+        stage('Deploy container image'){
+            steps{
+                withVault(configuration: [engineVersion: 2, vaultCredentialId: 'Jenkins_prod_approle', vaultUrl: "${env.VAULT_URL}"], 
+                        vaultSecrets: [[path: 'secret/scaleway/jenkins_deploy', 
+                                secretValues: [
+                                [envVar: 'REGISTRY_USER', vaultKey: 'registry_username'],
+                                [envVar: 'REGISTRY_PASS', vaultKey: 'registry_password'],
+                                [envVar: 'SCW_PROJECT_ID', vaultKey: 'project_id'],
+                                [envVar: 'SCW_NS_ID',      vaultKey: 'namespace_id']
+                                ]]]) {
+                                sh '''
+                                    export SCW_ACCESS_KEY="${REGISTRY_USER}"
+                                    export SCW_SECRET_KEY="${REGISTRY_PASS}"
+                                    export SCW_DEFAULT_PROJECT_ID="${SCW_PROJECT_ID}"
+                                    export SCW_DEFAULT_REGION="fr-par"
+                                    scw container container update name="${IMAGE_NAME}" \
+                                        namespace-id="${SCW_NS_ID}" \
+                                        image="${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}@${IMAGE_DIGEST}" \
+                                        redeploy=true
+                                '''
+                        }
+                }
+            }
+    }
+}
